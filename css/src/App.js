7968891c2c8aa30e9727524d288d75ba3c8b491c
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-06 19:21:30 -0300

    avancando com css nos componentes

--- css/src/App.js
+++ css/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import "./components/MyComponents.css";
+import MyComponents from './components/MyComponents';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>React com CSS</h1>
+      {/* CSS de Componente */}
+      <MyComponents/>
+      <p>Este paragrafo e do app.js</p>
     </div>
   );
 }
