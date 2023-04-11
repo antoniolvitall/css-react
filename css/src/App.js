626282383c5a023825359d773ef5a23a89d14673
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-11 20:33:53 -0300

    classes dinamicas

--- css/src/App.js
+++ css/src/App.js
@@ -7,6 +7,8 @@ function App() {
 const n = 15;
 const [name] = useState("Antonio");
 
+const redTitle = false;
+
   return (
     <div className="App">
       {/* CSS Global */}
@@ -20,6 +22,9 @@ const [name] = useState("Antonio");
       <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"}) }>CSS dinamico</h2>
       <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"}) }>CSS dinamico</h2>
       <h2 style={name === "Antoni" ? ({color: "green", backgroundColor: "#000"}) : null }>Teste nome</h2>
+      {/* classe dinamica */}
+      <h2 className={redTitle ? "red-title" : "title" }>Este titulo vai ter classe dinamica</h2>
+      
     </div>
   );
 }
